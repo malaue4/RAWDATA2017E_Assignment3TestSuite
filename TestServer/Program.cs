48742bb7e68c30f214e368261204c4b093566486
alexander <alexander@ibsen.me>
2017-10-11 13:40:28 +0200

    Checking request validity

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -26,7 +26,7 @@ namespace TestServer
         public string Name { get; set; }
     }
 
-        
+
     public class Program
     {
         static void Main(string[] args)
@@ -38,6 +38,10 @@ namespace TestServer
         private TcpListener _server;
         private List<Category> _categories;
         private bool isRunning;
+
+        private String[] methodNames = { "read", "create", "update", "delete","echo" };
+        private String pathPrefix = "/api/";
+        
 
         public Program()
         {
@@ -67,7 +71,7 @@ namespace TestServer
 
         public void StopServer()
         {
-            
+
             isRunning = false;
             _server.Stop();
         }
@@ -92,8 +96,86 @@ namespace TestServer
 
                 var responseData = Encoding.UTF8.GetString(memStream.ToArray());
                 Console.WriteLine(responseData.ToString());
-                //    return JsonConvert.DeserializeObject<Response>(responseData);
+                RequestObject obj = new RequestObject();
+                obj = JsonConvert.DeserializeObject<RequestObject>(responseData);
+                String response;
+                response = CheckValidity(obj);
+                Console.WriteLine(response);
+                //Console.WriteLine("Method: "+obj.method+" - Path: "+obj.path+" - Date: "+obj.date+" - Body: "+obj.body);
             }
         }
+
+        private String CheckValidity(RequestObject _obj)
+        {
+
+            String status=" ";
+            int statusCode = 1;
+                if(_obj.method == null)
+            {
+                status += "missing method,";
+                statusCode = 4;
+            } else if (!Array.Exists(methodNames, delegate(string s) { return s.Equals(_obj.method); }))
+                {
+                status += "illegal method,";
+                statusCode = 4;
+                }
+
+                if(_obj.path == null)
+            {
+                status += "missing path,";
+                statusCode = 4;
+            } else if (!_obj.path.StartsWith(pathPrefix))
+            {
+                status += "illegal path,";
+                statusCode = 4;
+            }
+            if (_obj.date == null)
+            {
+                status += "missing path,";
+                statusCode = 4;
+            }
+            else 
+            {
+                try
+                {
+                    int.Parse(_obj.date);
+                }
+                catch (Exception)
+                {
+
+                    status += "illegal date,";
+                    statusCode = 4;
+
+                }
+            }
+
+            if(_obj.body == null)
+            {
+
+                status += "missing body,";
+                statusCode = 4;
+            }
+            status = statusCode + status;
+            return status;
+
+        }
+        
+            
+        
     }
+    class RequestObject {
+
+        [JsonProperty("method")]
+        public String method;
+
+        [JsonProperty("path")]
+        public String path;
+
+        [JsonProperty("date")]
+        public String date;
+
+        [JsonProperty("body")]
+        public String body;
+
+        }
 }
