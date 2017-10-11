955fb4836731a4891bed82ac8eba633cf1fc7e1d
Martin Laursen <malaue4@gmail.com>
2017-10-11 23:04:38 +0200

    Flyttet util klassen til sin egen fil

---
+++ TestServer/Util.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace TestServer
+{
+    public static class Util
+    {
+
+        public static void SendResponse(this TcpClient client, string response)
+        {
+            var msg = Encoding.UTF8.GetBytes(response);
+            client.GetStream().Write(msg, 0, msg.Length);
+        }
+
+        public static Request ReadRequest(this TcpClient client)
+        {
+            var stream = client.GetStream();
+            //strm.ReadTimeout = 250;
+            byte[] resp = new byte[2048];
+            using (var memStream = new MemoryStream())
+            {
+                int bytesread = 0;
+                do
+                {
+                    bytesread = stream.Read(resp, 0, resp.Length);
+                    memStream.Write(resp, 0, bytesread);
+
+                } while (bytesread == 2048);
+
+                var responseData = Encoding.UTF8.GetString(memStream.ToArray());
+                return JsonConvert.DeserializeObject<Request>(responseData);
+            }
+        }
+
+        public static string ToJson(this object data)
+        {
+            return JsonConvert.SerializeObject(data,
+                new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+        }
+
+        public static T FromJson<T>(this string element)
+        {
+            return JsonConvert.DeserializeObject<T>(element);
+        }
+
+    }
+}
