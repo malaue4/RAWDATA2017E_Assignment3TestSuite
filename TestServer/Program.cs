dd74cbc977742f94fe9252558e369f33748c3c5c
Martin Laursen <malaue4@gmail.com>
2017-10-11 14:03:41 +0200

    util class

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -96,4 +96,34 @@ namespace TestServer
             }
         }
     }
+
+    public static class Util
+    {
+
+        public static void SendResponse(this TcpClient client, string response)
+        {
+            var msg = Encoding.UTF8.GetBytes(response);
+            client.GetStream().Write(msg, 0, msg.Length);
+        }
+
+        public static Request ReadRequest(this TcpClient client)
+        {
+            var strm = client.GetStream();
+            //strm.ReadTimeout = 250;
+            byte[] resp = new byte[2048];
+            using (var memStream = new MemoryStream())
+            {
+                int bytesread = 0;
+                do
+                {
+                    bytesread = strm.Read(resp, 0, resp.Length);
+                    memStream.Write(resp, 0, bytesread);
+
+                } while (bytesread == 2048);
+
+                var responseData = Encoding.UTF8.GetString(memStream.ToArray());
+                return JsonConvert.DeserializeObject<Response>(responseData);
+            }
+        }
+    }
 }
